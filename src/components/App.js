04ff655277fd4b432ae2966116265e1279561000
fhidesjoseph <fhidesjoseph@gmail.com>
2022-07-21 00:15:07 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, {useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [question, postQuestions]=useState([])
+  const postNewQuestion = (newQuestion) => {
+    postQuestions([...question, newQuestion])
+  }
 
+const onDelete= (data) => {
+  const getQuestion = question.filter(question => question.id !== data.id)
+  postQuestions(getQuestion)
+};
+ 
+
+  //const onUpdate = (item) => {
+   // const updateAnswer =  questionsList.map(question => {
+     // if(question.id === item.id) return item;
+     // return question;
+    //});
+   // setQuestionsList(updateAnswer)
+ // }
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm postNewQuestion={postNewQuestion}  /> : <QuestionList  onDelete={onDelete}  />}
     </main>
   );
 }
